83dbcc8d9b77ecfdaaf6662ffdc38c2f927bb739
Colson <colsoncalifornication@gmail.com>
2024-04-02 14:25:01 +0545

    send a user into the oauth flow: render auth status

--- client/src/components/GoogleAuth.jsx
+++ client/src/components/GoogleAuth.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  // The reason we're putting null here is that we do not know, if the user is
+  // signed in or not signed in when our application first loads
+  // We probably should not print anything on the screen at that point in time
+  state = { isSignedIn: null };
+
   /* @ Initialize the library */
   // Anytime that our component is rendered onto the screen, we're going to
   // load up the client portion of the library
@@ -15,21 +20,55 @@ class GoogleAuth extends React.Component {
     // gapi is available on window scope that is available inside of a browser
     // Load addiotional code to the gapi library: load client:auth2
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        // initialize authentication client with clientId
-        clientId: 'clientid',
-        // ask for the scope of email
-        scope: 'email',
-        // in order to use the Google Platform API, we have to pass a plugin_name
-        // as a parameter to the init method. The value is just an arbitrary name
-        // and can be anything we wish
-        plugin_name: 'twitchlite',
-      });
+      // client.init executes a asynchronous network request over to google api
+      // server inorder to initialize our client
+      // so we want to get some type of callback function or some time of notice
+      // for when that initialization setup is all done
+      // so to do so we are going to chain on a .then() statement
+      // The reason we have to use .then() is:
+      // When we call load('client:auth') there, load only allows us to get a
+      // signal or a notification of when the loading process is complete by
+      // passing in a callback function
+      // But when we call init here, we do not have to use a callback function
+      // When we call init, it returns a Promise. So by chaining a .then(),
+      // this arrow function inside .then() is going to be automatically invoked
+      // after a library has successfully initialize itself.
+      // So inside of .then(), we can write some amount of code that will be
+      // only executed once our entire gapi library is ready to go!
+      window.gapi.client
+        .init({
+          // initialize authentication client with clientId
+          clientId:
+            '11733010542-qo71645n5bbqsfardrvkf1idsgdo5afi.apps.googleusercontent.com',
+          // ask for the scope of email
+          scope: 'email',
+          // in order to use the Google Platform API, we have to pass a plugin_name
+          // as a parameter to the init method. The value is just an arbitrary name
+          // and can be anything we wish
+          plugin_name: 'twitchlite',
+        })
+        .then(() => {
+          // So at this point in time, we can start to figure out whether or not
+          // the user is currently signedin and then attempt to print out that
+          // status the screen.
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
 
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I dont know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 

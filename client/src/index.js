7d12279794d1ce8822c7b07280c6465e48049756
Colson <colsoncalifornication@gmail.com>
2024-04-04 09:50:31 +0545

    hook redux dev tools to the redux store + redux debug sessions

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+
+// Start a redux debug session
+// A debug session is where redux dev tools is going to automatically save
+// all of the data inside of a redux store and persist it across refreshes of
+// our application
+// Normally when we refresh our application, all the data inside of our redux
+// store, falls away. We lose it entirely
+// When we enable this debug session, Redux dev tools is going to make sure that
+// all the data stays around between refreshes of the page.
+// So this is incredebly useful for advanced feature development.
+/* localhost:3000?debug_session=<some_string> */
 
 ReactDOM.render(
   <Provider store={store}>

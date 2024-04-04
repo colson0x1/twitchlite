f653f80ab2cfe18494bc10806f9a62e67811cd1f
Colson <colsoncalifornication@gmail.com>
2024-04-04 12:49:23 +0545

    connect redux form + refactor stream to class component + use reduxForm function

--- client/src/components/streams/StreamCreate.jsx
+++ client/src/components/streams/StreamCreate.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+// reduxForm is a function that's going to essentially have the exact same
+// functionality as the `connect()` function that we used from the `react-redux`
+// library
+// The reduxForm function is essentially what's going to make sure that we can
+// call some action creator and get some form data into our component. Cool thing
+// is that all happens automatically
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+// The reason we're going to make this Class based Component is that we're
+// going to eventually want to have a bunch of helper methods so we kind kindof
+// better organize our code inside of here!
+class StreamCreate extends React.Component {
+  render() {
+    // console.log('StreamCreate', this.props);
+    return (
+      <form>
+        <Field name='title' />
+        <Field name='description' />
+      </form>
+    );
+  }
+}
 
-export default StreamCreate;
+// reduxForm is going to return a function and we immediately call that function
+// with StreamCreate
+// reduxForm receives a single object and we put bunch of configuration into
+// that object
+export default reduxForm({
+  // name of this `form` is generally whatever the purpose of the form is
+  form: 'streamCreate',
+})(StreamCreate);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,17 @@
 import { combineReducers } from 'redux';
+// renaming reducer to formReducer to prevent confusion because we might have
+// lots of reducers inside this file and it might be confusing to know
+// what reducer it is
+import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 
+/* @ Redux Form
+https://redux-form.com/7.4.2/examples/syncvalidation/
+*/
 export default combineReducers({
   // assigning a key of auth for auth reducer
   auth: authReducer,
+  // This is going to be a reducer that is been created for us by the
+  // Redux form library
+  form: formReducer,
 });

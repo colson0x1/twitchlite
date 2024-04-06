f848b84c5aba97b99a4f0e2cb79a10e4d4491013
Colson <colsoncalifornication@gmail.com>
2024-04-06 10:02:27 +0545

    hookup onsubmit event handler using redux form

--- client/src/components/streams/StreamCreate.jsx
+++ client/src/components/streams/StreamCreate.jsx
@@ -43,16 +43,44 @@ class StreamCreate extends React.Component {
     );
   }
 
+  onSubmit(formValues) {
+    // No longer we're dealing with event object as our argument here.
+    // The event object is really not useful to us one bit
+    // The only reason we dealt with the event object is so that we can call
+    // preventDefault method
+    // Instead now its going to get called with all the values out of our form
+    // inside of an object
+    console.log(formValues);
+  }
+
   render() {
     // console.log('StreamCreate', this.props);
+    // `this.props.handleSubmit` is a callback function provided by Redux Form
+    // we then call that function with our call back method that we defined inside
+    // of our component
+    // now internally, handleSubmit on this.props.handleSubmit is now automatically
+    // receives event object and automatically call preventDefault() for us.
+    // Redux Form takes care of it for us.
+    // In fact onSubmit is not even going to be called with an `event` object
+    // at all. Instead, its going to be called with whatever values existed
+    // inside of our two field inputs
+    // usually we refer to that argument as `formProps`
+    // Now formProps does not mean that this is some data passed down by a
+    // parent component. The better name for this argument is `formValues`
+    // In simple, we reference handleSubmit and pass in whatever callback
+    // we want to run after the form gets submitted.
+    // So in relality, handleSubmit is going to be called and once it processes
+    // the form event and all that good stuff, our callback then gets invoked
+    // with our actual form values that we care about.
     return (
-      <form>
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
         <Field name='title' component={this.renderInput} label='Enter Title' />
         <Field
           name='description'
           component={this.renderInput}
           label='Enter Description'
         />
+        <button className='ui button primary'>Submit</button>
       </form>
     );
   }

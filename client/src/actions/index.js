133254b24ebcfab0ba38ba11a3fa90af0df55db9
Colson <colsoncalifornication@gmail.com>
2024-04-08 18:10:53 +0545

    implement all action creators for each of the different operations (listing, creating, updating, deleting, retrieving particular record)

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from './types';
 
 export const signIn = (userId) => {
   return {
@@ -24,4 +32,36 @@ export const createStream = (formValues) => async (dispatch) => {
   // about the response. But we only care about the information that was
   // returned inside the request. So, returning `response.data`
   dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+// List all records i.e streams
+export const fetchStreams = () => async (dispatch) => {
+  // Using axios instance
+  const response = await streams.get('/streams');
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+// Get one particular stream
+export const fetchStream = (id) => async (dispatch) => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+// Update a stream
+export const editStream = (id, formValues) => async (dispatch) => {
+  // pass id + make sure we communicate the update that we want to make
+  // on the body of the request
+  const response = await streams.put(`/streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+// Delete a stream
+export const deleteStream = (id) => async (dispatch) => {
+  // response is going to be empty on this case since its a delete request
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,12 @@
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
+// Create a stream
 export const CREATE_STREAM = 'CREATE_STREAM';
+// List all records i.e streams
+export const FETCH_STREAMS = 'FETCH_STREAMS';
+// Get one particular stream
+export const FETCH_STREAM = 'FETCH_STREAM';
+// Delete a stream
+export const DELETE_STREAM = 'DELETE_STREAM';
+// Update a stream
+export const EDIT_STREAM = 'EDIT_STREAM';

769f72c5d5f0ac26c7fe04f35f4d595d9fe159fc
Colson <colsoncalifornication@gmail.com>
2024-04-09 12:16:48 +0545

    link to create stream

--- client/src/components/streams/StreamList.jsx
+++ client/src/components/streams/StreamList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 import { fetchStreams } from '../../actions';
 
 // Using Class based Component because we want to call our action creator
@@ -45,6 +47,18 @@ class StreamList extends React.Component {
     });
   }
 
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Link to='/streams/new' className='ui button primary'>
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
+  }
+
   render() {
     // Test if we received streams successfully
     // console.log(this.props.streams);
@@ -52,6 +66,7 @@ class StreamList extends React.Component {
       <div>
         <h2>Streams</h2>
         <div className='ui celled list'>{this.renderList()}</div>
+        {this.renderCreate()}
       </div>
     );
   }
@@ -95,6 +110,7 @@ const mapStateToProps = (state) => {
     // bit more obvious that this is the id of the person was currently signed in and
     // using the application.
     currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn,
   };
 };
 

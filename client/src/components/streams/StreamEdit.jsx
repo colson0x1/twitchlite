12116f879b0e47997ad4017d37ba0c83c5954593
Colson <colsoncalifornication@gmail.com>
2024-04-13 14:52:06 +0545

    component isolation with react router + fetch a stream for edit stream

--- client/src/components/streams/StreamEdit.jsx
+++ client/src/components/streams/StreamEdit.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
+
+// With React Router, each component needs to be designed to work by itself or
+// in isolation (fetch its own data!)
+// Essentially that means that every component that React Router DOM is gonna show
+// needs to fetch its own data
+// We really cannot assume that any given component will get access to some
+// data that might have been loaded up previously inside of our application
+// If a user navigates directly to /streams/edit/streamId like 3, there are no
+// stream to select from
+// So our stream edit component needs to load up appropriate stream
+// It needs to call the action creator to react out to the api and fetch the
+// stream with id say 3 so then it can show it on the screen
+// Thats' the big lesson here.
+// WE just need to make sure every component is not going to have some dependency
+// on a user visiting some other route inside of our application ahead of time
+// Like if the user bookmarks /streams/edit/3 page then that would completely
+// bypass the stream list component. In addition, a user might copy that same
+// link /streams/edit/3 right here and share with a friend. the friend click
+// the url, come to this page and expects to see the appropriate stream up here.
+// So its entirely possible that our users are always going to come directly
+// to a given route inside of our application so every route and every component
+// shown by that route needs to fetch its own data and it cannot rely upon some
+// other component fetching data ahead of time for it.
 
 // We have now access to some props specifically because, the StreamEdit
 // component inside of our App Component is being rendered by a Route Component
@@ -7,10 +31,21 @@ import { connect } from 'react-redux';
 // going to add in a bunch of different props to StreamEdit when it gets
 // rendered onto the screen
 // Those props are history, location, and match
-const StreamEdit = (props) => {
-  console.log(props);
-  return <div>StreamEdit</div>;
-};
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    // console.log(this.props);
+
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
+    return <div>{this.props.stream.title}</div>;
+  }
+}
 
 // Our component knows about the id of the stream that we're trying to show
 // on the screen but we don't want to have access to that id inside of our
@@ -28,4 +63,4 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[streamId] };
 };
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);

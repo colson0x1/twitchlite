c40a15ea49789626d0d995a5ee73e3731b35765e
Colson <colsoncalifornication@gmail.com>
2024-04-15 12:42:43 +0545

    make modal reusable: react fragments

--- client/src/components/streams/StreamDelete.jsx
+++ client/src/components/streams/StreamDelete.jsx
@@ -2,10 +2,28 @@ import React from 'react';
 import Modal from '../Modal';
 
 const StreamDelete = () => {
+  const actions = (
+    // The only time we're going to use React.Fragment instead of <></>
+    // is that, there is some tooling out there like some linter, some
+    // code quality checkers that do  not think that empty tags like that
+    // are a valid syntax.
+    // So some code quality checkers that we might be running even inside of our
+    // own code editor will think that  this is invalid JSX and throw an error
+    // and warns, hey this is not valid, you're doing something wrong here.
+    <React.Fragment>
+      <button className='ui button negative'>Delete</button>
+      <button className='ui button'>Cancel</button>
+    </React.Fragment>
+  );
+
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal
+        title='Delete Stream'
+        content='Are you sure you want to delte this stream?'
+        actions={actions}
+      />
     </div>
   );
 };

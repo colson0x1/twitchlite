8290597d0594f60ab1a7c5e2e504612e989b4e8f
Colson <colsoncalifornication@gmail.com>
2024-04-16 12:56:55 +0545

    show a stream

--- client/src/components/streams/StreamShow.jsx
+++ client/src/components/streams/StreamShow.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+class StreamShow extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
+    const { title, description } = this.props.stream;
+
+    return (
+      <div>
+        <h1 style={{ color: 'orangered' }}>{title}</h1>
+        <h4 style={{ color: 'gray' }}>{description}</h4>
+      </div>
+    );
+  }
+}
+
+// get that stream out of our Redux store and get it inside of our component
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamShow;
+export default connect(mapStateToProps, { fetchStream })(StreamShow);

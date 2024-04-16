70b0d347f4c3efb2a1b46cd53c4178d672f32c7a
Colson <colsoncalifornication@gmail.com>
2024-04-16 15:03:56 +0545

    create a flv player + configure player building

--- client/src/components/streams/StreamShow.jsx
+++ client/src/components/streams/StreamShow.jsx
@@ -3,6 +3,20 @@ import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
+/* overview: make the video player somehow request video to come from RTMP server
+ * In order to do so, we're going to use FLV package.
+ * FLV is about downloading that video stream and converting it to some file
+ * that can actually be played inside of this normal html video player.
+ * We can think, FLV js a little bit more like axios. In that its going to
+ * reach out to some remote server, get some resource and serve up that data
+ * to our application to be consumed on the screen.
+ */
+
+/* @ OBS Configuration
+ * Stream Type: Custom Streaming Server
+ * URL: rmtp://localhost/live
+ * Stream key: STREAM_NAME (here Stream ID for us)
+ */
 class StreamShow extends React.Component {
   constructor(props) {
     super(props);
@@ -11,7 +25,61 @@ class StreamShow extends React.Component {
   }
 
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+    console.log(this.videoRef);
+
+    this.props.fetchStream(id);
+    // We only want to setup this video player right here after we know that
+    // this appropriate stream has been actually fetched.
+    // Hence, this is not the right location!
+    /* this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load(); */
+
+    // So now, whenever our component is first rendered, we're going to attempt
+    // to fetch our streams. We're also gonna try to build the player.
+    // So we're going to call buildPlayer() if we have not built the player
+    // before and if we do not have stream, we're going to return.
+    // So that's going to take care of the case of us first loading the page
+    // and not yet having access to the actual stream.
+    this.buildPlayer();
+  }
+
+  // Just in case we do not yet have access to our stream and we're going to
+  // try to fetch it on the fly, we need to have some place to call buildPlayer
+  // after we successfully fetched the stream!
+  // Now the idea is that, when our component first renders, attempt to build
+  // the player and then if our component fetches the stream successfully at
+  // some point of time in he future and the component rerenders,
+  // componentDidUpdate() will be called and we'll attempt to call build player
+  // in there as well.
+  // So essentially, at any point that this component gets rerendered, either
+  // the intial render or any follow-up render, we're always going to attempt
+  // to build a player.
+  // But if we already built it and we already have a player OR if we do not
+  // have the stream, then we're not going to try to build it.
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
+
+    // We only want to setup this video player right here after we know that
+    // this appropriate stream has been actually fetched.
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
